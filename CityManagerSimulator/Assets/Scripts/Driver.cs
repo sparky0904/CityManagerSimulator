c136b3147fe4925bcfddd955b224e447752bac2b
Sparky0904 <sparky0904@gmail.com>
2018-06-13 21:01:45 +0100

    AI fundamentals
    Added sensors, drives forard and brakes if front center sensor fires

--- CityManagerSimulator/Assets/Scripts/Driver.cs
+++ CityManagerSimulator/Assets/Scripts/Driver.cs
@@ -2,18 +2,56 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Driver : MonoBehaviour {
+[RequireComponent(typeof(myWheelDrive))]
+public class Driver : MonoBehaviour
+{
+    private Vehicle vehicle;
+    private myWheelDrive wheelDrive;
 
-    Vehicle vehicle;
+    private float newBrakeForceAmount;
+    private float newAcceleratorForceAmount;
+    private float newTurnAngleAmount;
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    private void Start()
+    {
         vehicle = GetComponent<Vehicle>();
-	}
-	
-	// Update is called once per frame
-	void Update () {
+        wheelDrive = GetComponent<myWheelDrive>();
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
         vehicle.Accelerator = Input.GetAxis("Vertical");
         vehicle.TurnAngle = Input.GetAxis("Horizontal");
-	}
+    }
+
+    private void FixedUpdate()
+    {
+        CheckSensors();
+        Drive();
+    }
+
+    private void CheckSensors()
+    {
+        SensorsInfoStruct sensorData = vehicle.SensorsInfo;
+
+        // check if something is in front of us and depending on the distance apply the brakes
+        if (sensorData.frontCenterSensorFired)
+        {
+            newBrakeForceAmount = 1; // Just slow down ASAP
+            newAcceleratorForceAmount = 0;
+        }
+        else
+        {
+            newAcceleratorForceAmount = 1;
+            newBrakeForceAmount = 0;
+        }
+    }
+
+    private void Drive()
+    {
+        vehicle.Accelerator = newAcceleratorForceAmount;
+        vehicle.Brake = newBrakeForceAmount;
+    }
 }
--- CityManagerSimulator/Assets/Scripts/Vehicle.cs
+++ CityManagerSimulator/Assets/Scripts/Vehicle.cs
@@ -2,14 +2,38 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+public struct SensorsInfoStruct
+{
+    public RaycastHit frontCenterSensor;
+    public RaycastHit frontLeftSensor;
+    public RaycastHit frontLeftAngleSensor;
+    public RaycastHit frontRightSensor;
+    public RaycastHit frontRightAngleSensor;
+
+    public bool frontCenterSensorFired;
+    public bool frontLeftSensorFired;
+    public bool frontLeftAngleSensorFired;
+    public bool frontRightSensorFired;
+    public bool frontRightAngleSensorFired;
+}
+
 [RequireComponent(typeof(Driver))]
 public class Vehicle : MonoBehaviour
 {
     private Rigidbody rb;
-    private float speed;    
+    private float speed;
     private float accelerator;
     private float brake;
     private float turnAngle;
+
+    [Header("Sensors")]
+    public float sensorLength = 10f;
+
+    public Vector3 frontSensorPosition = new Vector3(0f, 0f, 0.5f);
+    public float frontSideSensorPosition = 0.2f;
+    public float frontSensorAngle = 30f;
+
+    public SensorsInfoStruct SensorsInfo;
 
     public float Accelerator
     {
@@ -34,17 +58,84 @@ public class Vehicle : MonoBehaviour
     {
         rb = GetComponent<Rigidbody>();
 
-        // Ensure care is stationary
+        // Ensure car is stationary
         speed = 0;
         accelerator = 0;
         brake = 0;
     }
 
     // Update is called once per frame
-    private void Update()
+    private void FixedUpdate()
     {
+        ReadSensors();
+
         speed = rb.velocity.magnitude;
         Debug.Log(rb.velocity.magnitude * 2.237);  // 2.237 is from wolframalpha.com found on a unity webpage
     }
 
+    private void ReadSensors()
+    {
+        RaycastHit hit;
+        Vector3 sensorStartPos = transform.position + frontSensorPosition;
+
+        SensorsInfo.frontCenterSensorFired = false;
+        SensorsInfo.frontLeftAngleSensorFired = false;
+        SensorsInfo.frontLeftSensorFired = false;
+        SensorsInfo.frontRightSensorFired = false;
+        SensorsInfo.frontRightAngleSensorFired = false;
+
+        // Front center Sensor
+        if (Physics.Raycast(sensorStartPos, transform.forward, out hit, sensorLength))
+        {
+            Debug.DrawLine(sensorStartPos, hit.point);
+            // Update the sensor with new data
+            SensorsInfo.frontCenterSensor = hit;
+            SensorsInfo.frontCenterSensorFired = true;
+            // Fire an event
+        }
+
+        // Front right sensor
+        sensorStartPos.x += frontSideSensorPosition;
+        if (Physics.Raycast(sensorStartPos, transform.forward, out hit, sensorLength))
+        {
+            Debug.DrawLine(sensorStartPos, hit.point);
+            // Update the sensor with new data
+            SensorsInfo.frontRightSensor = hit;
+            SensorsInfo.frontRightSensorFired = true;
+            // Fire an event
+        }
+
+        // Front right angle sensor
+        if (Physics.Raycast(sensorStartPos, Quaternion.AngleAxis(frontSensorAngle, transform.up) * transform.forward, out hit, sensorLength))
+        {
+            Debug.DrawLine(sensorStartPos, hit.point);
+            // Update the sensor with new data
+            SensorsInfo.frontRightAngleSensor = hit;
+            SensorsInfo.frontRightAngleSensorFired = true;
+
+            // Fire an event
+        }
+
+        // Front left sensor
+        sensorStartPos.x -= 2 * frontSideSensorPosition;
+        if (Physics.Raycast(sensorStartPos, transform.forward, out hit, sensorLength))
+        {
+            Debug.DrawLine(sensorStartPos, hit.point);
+            // Update the sensor with new data
+            SensorsInfo.frontLeftSensor = hit;
+            SensorsInfo.frontLeftSensorFired = true;
+            // Fire an event
+        }
+
+        // Front left angle sensor
+        if (Physics.Raycast(sensorStartPos, Quaternion.AngleAxis(-frontSensorAngle, transform.up) * transform.forward, out hit, sensorLength))
+        {
+            Debug.DrawLine(sensorStartPos, hit.point);
+            // Update the sensor with new data
+            SensorsInfo.frontLeftAngleSensor = hit;
+            SensorsInfo.frontLeftAngleSensorFired = true;
+
+            // Fire an event
+        }
+    }
 }
